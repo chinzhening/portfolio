18136db7eff7e529c9b30c94ddd4a55b9772888a
Chin Zhe Ning <ning.chin03@gmail.com>
2026-05-31 12:07:20 +0800

    fix: resume.pdf is  resolved

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,7 +39,7 @@
                             </p>
                         </div>
 
-                        <TagChip variant="postcard" href="/resume.pdf" download="resume.pdf">
+                        <TagChip variant="postcard" href={resolve("/resume.pdf")} download="resume.pdf">
                             <span>download</span>
                             <span>resume</span>
                             <svg class="inline-block mt-1" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false">
